e07565f87dc4810e494b08a0325809379dcde060
saurabh <saurabhj8087445@gmail.com>
2022-04-10 16:27:00 +0530

    add app.js file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import AddUser from "./components/AddUser";
+import Album from "./components/Album";
 
-function App() {
+const App = () => {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    await fetch(" https://jsonplaceholder.typicode.com/albums")
+      .then((res) => res.json())
+      .then((data) => setUsers(data))
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const onAdd = async (name) => {
+    await fetch("https://jsonplaceholder.typicode.com/albums", {
+      method: "POST",
+      body: JSON.stringify({
+        title: name,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((res) => {
+        if (res.status !== 201) {
+          return;
+        } else {
+          return res.json();
+        }
+      })
+      .then((data) => {
+        setUsers((users) => [...users, data]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const onDelete = async (id) => {
+    await fetch(`https://jsonplaceholder.typicode.com/albums/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => {
+        if (res.status !== 200) {
+          return;
+        } else {
+          setUsers(
+            users.filter((user) => {
+              return user.id !== id;
+            })
+          );
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  console.log(users);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Albums list React app</h3>
+
+      <br />
+      <AddUser onAdd={onAdd} />
+      <div className="USer-Wraper">
+        {users.map((user) => (
+          <Album
+            id={user.id}
+            key={user.id}
+            name={user.title}
+            onDelete={onDelete}
+          />
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
